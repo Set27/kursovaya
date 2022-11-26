d250e4faf27bbc4e00e99f8d19e81445887dfa29
Set27 <matvei.2002@mail.ru>
2022-11-26 17:33:46 +0100

    Add page for wanted list models

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def navbar_link_item(title:, path: nil)
+    if path.nil?
+      link_to title, '#', class: "nav-link disabled"
+    elsif current_page?(path)
+      link_to title, path, class: "nav-link active"
+    else
+      link_to title, path, class: "nav-link"
+    end
+  end
 end
--- db/migrate/20221124130504_add_ref.rb
+++ db/migrate/20221124130504_add_ref.rb
@@ -1,6 +1,5 @@
 class AddRef < ActiveRecord::Migration[7.0]
   def change
-    rename_table :registration_infos, :registation_infos
     change_table :wanted_lists do |t|
       t.references :registation_info, null: true, foreign_key: true
     end

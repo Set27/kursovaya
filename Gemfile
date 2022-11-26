7c024220ea670e481ab782479d216ec880a48b01
Roman Dubrovsky <r.dubrovsky@datarockets.com>
2022-11-26 18:26:32 +0100

    Add pages for car info

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem "puma", "~> 5.0"
 gem "importmap-rails"
 
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
-gem "turbo-rails"
+# gem "turbo-rails"
 
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
@@ -76,3 +76,4 @@ group :test do
 end
 
 gem 'simple_form'
+gem 'webpacker', github: 'rails/webpacker'
---
+++ app/controllers/car_infos_controller.rb
@@ -0,0 +1,50 @@
+class CarInfosController < ApplicationController
+  before_action :find_car, except: [:index, :new, :create]
+
+  def index
+    @cars = CarInfo.all
+  end
+
+  def new
+    @car = CarInfo.new
+  end
+
+  def create
+    @car = CarInfo.new(car_params)
+    if @car.valid?
+      @car.save
+      redirect_to action: :index
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @car.assign_attributes(car_params)
+
+    if @car.valid?
+      @car.save
+      redirect_to action: :index
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @car.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def find_car
+    @car = CarInfo.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car_info).permit(:VIN, :brand, :model)
+  end
+end

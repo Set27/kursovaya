7a7e6e528e7a1d6cc7af861342085f1505b14497
Set27 <matvei.2002@mail.ru>
2022-11-27 15:27:31 +0300

    add all pages

--- app/controllers/fines_controller.rb
+++ app/controllers/fines_controller.rb
@@ -1,2 +1,56 @@
 class FinesController < ApplicationController
+  before_action :find_fine, except: [:index, :new, :create]
+  before_action :authorize_fine
+
+    def index
+        @fines = Fine.all
+    end
+    
+      def new
+        @fine = Fine.new
+      end
+    
+      def create
+        @fine = Fine.new(fine_params)
+        if @fine.valid?
+          @fine.save
+          redirect_to action: :index
+        else
+          render 'new'
+        end
+      end
+    
+      def edit
+      end
+    
+      def update
+        @fine.assign_attributes(fine_params)
+    
+        if @fine.valid?
+          @fine.save
+          redirect_to action: :index
+        else
+          render 'edit'
+        end
+      end
+    
+      def destroy
+        @fine.destroy
+        redirect_to action: :index
+      end
+
+  private
+
+  def authorize_fine
+    authorize!(@fine || Fine.new)
+  end
+
+  def find_fine
+    @fine = Fine.find(params[:id])
+  end
+
+  def fine_params
+    params.require(:fine).permit(:date_of_recepeit,:article,:amount)
+  end
+
 end
